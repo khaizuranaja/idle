f8a4a797450671ba662dec0d590434256efc7287
danish ae bro <117614560+khaizuranaja@users.noreply.github.com>
2022-11-08 07:38:46 +0700

    Update itjay.py

--- itjay.py
+++ itjay.py
@@ -1,5 +1,26 @@
-danish = "danish keren"
+danish = "danish-ssi"
 print (danish)
 
 ###soal1
-print (1+1)
+
+PSY = "Hello World to Python"
+print (PSY)
+
+###soal2
+
+print (1+2)
+print (2-1)
+print (3*5)
+print (15//3)
+
+###soal3
+
+ryu1 = (72)
+ryu2 = (12)
+ryu3 = (ryu1 / ryu2)
+ryu4 = (ryu1 // ryu2)
+
+print (ryu3)
+print (ryu4)
+
+###soal4
