57faa241d5c46c738accf32f44b8e7ab2dd40011
unknown <mkdanish17@gmail.com>
2022-11-08 07:51:49 +0700

    danishketjeh

--- itjay.py
+++ itjay.py
@@ -1,5 +1,32 @@
-danish = "danish keren"
+danish = "danish-ssi"
 print (danish)
 
 ###soal1
-print (1+1)
+
+PSY = "Hello World to Python"
+print (PSY)
+
+###soal2
+
+print (1+2)
+print (2-1)
+print (3*5)
+print (15//3)
+
+###soal3
+
+ryu1 = (72)
+ryu2 = (12)
+ryu3 = (ryu1 / ryu2)
+ryu4 = (ryu1 // ryu2)
+
+print (ryu3)
+print (ryu4)
+
+###soal4
+
+jin5 = "usiaku:"
+jin6 = "16 Tahun"
+
+print (jin5, jin6)
+
